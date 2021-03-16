c37e8a15f5650e07701e800875f3b80f3a4e3d9d
Zahreddine Elbouzidi <elbouzidi.zahreddine@gmail.com>
2021-03-17 00:17:53 +0100

    third commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Open and close sidebar
+function open_sidebar() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function close_sidebar() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
