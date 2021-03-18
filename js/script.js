42727db095c33d93b5ef60218dfda91f93fc0a33
Zahreddine Elbouzidi <elbouzidi.zahreddine@gmail.com>
2021-03-18 18:39:48 +0100

    changes index and Javascript file menu commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,11 @@
  
-// Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
-
-  var x = document.getElementById("mySidebar");
-
-  var bar_icone = document.getElementById("bar_icone");
-
-  if (x.className.indexOf("w3-show") == -1) {
-
-    x.className += " w3-show";
-
-    bar_icone.className = bar_icone.className.replace("fa fa-bars","fa fa-remove");
-
-  } else {
-
-      x.className = x.className.replace(" w3-show", "");
-
-      bar_icone.className = bar_icone.className.replace("fa fa-remove","fa fa-bars");
-  }
+// Open and close sidebar
+function menu_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function menu_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
 }
