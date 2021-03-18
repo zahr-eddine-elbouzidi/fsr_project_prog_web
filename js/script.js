b7908178cdbd37d5ab495465d778a0408f2e494a
Zahreddine Elbouzidi <elbouzidi.zahreddine@gmail.com>
2021-03-18 17:49:24 +0100

    index, style and script files changes commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-// Open and close sidebar
-function open_sidebar() {
-    document.getElementById("mySidebar").style.width = "100%";
-    document.getElementById("mySidebar").style.display = "block";
+ 
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+
+  var x = document.getElementById("mySidebar");
+
+  var bar_icone = document.getElementById("bar_icone");
+
+  if (x.className.indexOf("w3-show") == -1) {
+
+    x.className += " w3-show";
+
+    bar_icone.className = bar_icone.className.replace("fa fa-bars","fa fa-remove");
+
+  } else {
+
+      x.className = x.className.replace(" w3-show", "");
+
+      bar_icone.className = bar_icone.className.replace("fa fa-remove","fa fa-bars");
   }
-  
-  function close_sidebar() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
+}
